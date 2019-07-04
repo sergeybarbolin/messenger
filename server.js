b1363af7d083c3ca647c9d6675ccbc02f3ac5377
Sergey Barbolin <sergeybarbolin25@gmail.com>
2019-07-04 16:27:15 +0300

    log-in/out users

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ const path = require('path');
 const server = require('http').Server(app);	
 const io = require('socket.io')(server);
 
+const users = {}
+
+global.users = users;
+
 server.listen(3000, () => {
 	console.log('port 3000');
 });
@@ -15,17 +19,23 @@ app.get('/', function (req, res) {
 app.use('/static', express.static(path.join(__dirname, 'static')));
 
 io.on('connection', socket => {
-	console.log('user conected');
+	
+	socket.on('login', (user, users = global.users) => {
+		user.key = socket.id;
+		users[socket.id] = user;
+		io.emit('login', user, users);
 
-	socket.on('user', user => {
-		console.log(user, socket.id);
+		console.log(`${user.nickname} conected`);
 	})
 
-	socket.on('chat message', msg => {
-		io.emit('chat message', msg);
+	socket.on('chat message', message => {
+		io.emit('chat message', { value: message, user: global.users[socket.id] });
 	})
 
-	socket.on('disconnect', () => {
-		console.log('user disconected');
+	socket.on('disconnect', key => {
+		// delete global.users[socket.id];
+		io.emit('disconnect', socket.id);
+
+		console.log(`${global.users[socket.id].nickname} disconnected`);
 	})
 })
--- static/app.js
+++ static/app.js
@@ -1,20 +1,83 @@
-const form = document.querySelector('form');
-const input = form.querySelector('#m');
-const messages = document.querySelector('#messages');
-const user = 'sergey';
+const formSendMessage = document.querySelector('#send-message');
+const input = formSendMessage.querySelector('#m');
+const messages = document.getElementById('messages');
+const usersList = document.getElementById('users');
 
-form.addEventListener('submit', e => {
+let statusLogIn = false;
+let socket = null;
+
+const formLogIn = document.querySelector('#log-in');
+
+const formHandler = formIdentifier => {
+    const form = document.querySelector(formIdentifier);
+    const data = {};
+
+    for (const el of form.children) {
+        if (el.tagName === 'INPUT' || el.tagName === 'TEXTAREA') {
+            data[el.getAttribute('name')] = el.value
+        } 
+    }
+    
+    return data;
+}
+
+const socketLogin = (socket, user) => {
+	socket.emit('login', user);
+
+	socket.on('chat message', message => {
+	    const messageItem = document.createElement('li');
+
+	    console.log(message);
+	    messageItem.innerText = `${message.user.nickname}: ${message.value}`;
+	    messages.append(messageItem);
+	})
+
+	socket.on('login', (user, users) => {
+		if (statusLogIn) {
+			const userItem = document.createElement('li');
+
+		    userItem.setAttribute('data-key', user.key);
+		    userItem.innerText = user.name;
+		    usersList.append(userItem);
+		} else {
+			Object.keys(users).forEach(key => {
+				const userItem = document.createElement('li');
+
+			    userItem.setAttribute('data-key', key);
+			    userItem.innerText = users[key].name;
+			    usersList.append(userItem);
+			})
+			statusLogIn = true;
+		}
+	})
+
+	socket.on('disconnect', key => {
+		const disconnectedUser = usersList.querySelector(`li[data-key="${key}"]`);
+
+		if (disconnectedUser) {
+			disconnectedUser.remove();
+		}
+		statusLogIn = false;
+	})
+}
+
+formSendMessage.addEventListener('submit', e => {
     e.preventDefault();
     socket.emit('chat message', input.value);
     input.value = '';
     return false;
 });
 
-socket.emit('user', user);
+formLogIn.addEventListener('submit', e => {
+    e.preventDefault();
 
-socket.on('chat message', msg => {
-    const message = document.createElement('li');
+    const formData = formHandler('#log-in');
 
-    message.innerText = msg;
-    messages.append(message);
-})
+    socket = io.connect('http://localhost:3000');
+    socketLogin(socket, formData);
+
+    return false;
+});
+
+
+
