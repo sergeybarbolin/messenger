0e7dff1b94ff024f17414f1e92299deacbb668ae
Sergey Barbolin <sergeybarbolin25@gmail.com>
2019-07-04 09:11:00 +0300

    static files

--- server.js
+++ server.js
@@ -1,17 +1,18 @@
-var express = require('express');
-var app = express();
-var server = require('http').Server(app);	
-var io = require('socket.io')(server);
+const express = require('express');
+const app = express();
+const path = require('path');
+const server = require('http').Server(app);	
+const io = require('socket.io')(server);
 
 server.listen(3000, () => {
 	console.log('port 3000');
 });
 
 app.get('/', function (req, res) {
-  res.sendFile(__dirname + '/index.html');
+ 	res.sendFile(__dirname + '/index.html');
 });
 
-app.use(express.static('/static'));
+app.use('/static', express.static(path.join(__dirname, 'static')));
 
 io.on('connection', socket => {
 	console.log('user conected');
