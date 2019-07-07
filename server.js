ce6e185838b145c108ef3d02b18987277a4a85f0
sergeybarbolin <sergeybarbolin25@gmail.com>
2019-07-07 21:55:10 +0300

    change img

--- server.js
+++ server.js
@@ -23,25 +23,40 @@ app.use('/static', express.static(path.join(__dirname, 'static')));
 io.on('connection', socket => {
 	
 	socket.on('login', (user, users = global.users, messages) => {
-		if (!Object.keys(users).length || !users[user.nickname] || users[user.nickname].nickname === user.nickname && users[user.nickname].name === user.name) {
+		if (!Object.keys(users).length || !users[user.nickname]) {
 			users[user.nickname] = user;
-			console.log(`${user.nickname} conected`);
-			user.key = socket.id;
-			user.online = true;
-
-			const onlineUsers = Object.values(users).filter(item => item.online);
-			const filterMessages = global.messages.filter(msg => users[msg.user.nickname].online);
-
-			io.emit('login', user, onlineUsers, filterMessages);
+		} else if (users[user.nickname].nickname === user.nickname && users[user.nickname].name === user.name) {
+			// users[user.nickname].nickname = user.nickname;
+			// users[user.nickname].name = user.name;
 		} else {
 			console.log('Имя не совпадает с ником');
+			return;
 		}
+		console.log(`${user.nickname} conected`);
+		users[user.nickname].key = socket.id;
+		users[user.nickname].online = true;
+
+		const onlineUsers = Object.values(users).filter(item => item.online);
+		const filterMessages = global.messages.filter(msg => users[msg.user.nickname].online);
+
+		io.emit('login', users[user.nickname], onlineUsers, filterMessages);
 	})
 
 	socket.on('chat message', message => {
 		global.messages.push(message);
 
 		io.emit('chat message', message);	
+	})
+
+	socket.on('change img', (user, img) => {
+		global.users[user].img = img;
+		global.messages.forEach(msg => {
+			if (msg.user.nickname === user) {
+				msg.user.img = img;
+			}
+		});
+
+		socket.broadcast.emit('change img', global.users[user], img);
 	})
 
 	socket.on('disconnect', (key, messages, users = global.users) => {
--- static/app.js
+++ static/app.js
@@ -2,13 +2,18 @@ const formSendMessage = document.querySelector('#send-message');
 const inputMessage = formSendMessage.querySelector('#m');
 const messagesList = document.getElementById('messages');
 const usersList = document.getElementById('users');
-const popup = document.querySelector('.popup');
+const popupLogIn = document.querySelector('#popup-login');
 const formLogIn = document.querySelector('#log-in');
 
 const currentUserTamplate = document.querySelector('.user--current');
 const currentUserNameTamplate = currentUserTamplate.querySelector('.user__name');
 const countUsersTamplate = document.querySelector('.users__count');
 const usersTitle = document.querySelector('.users__title');
+const userImg = document.getElementById('user-img');
+
+const popupFileLoad = document.querySelector('#popup-file-load');
+const fileReader = new FileReader();
+const dropZone = document.getElementById('drop_zone');
 
 let socket = null;
 let currentUser = null;
@@ -26,93 +31,112 @@ const formHandler = formIdentifier => {
     return data;
 }
 
-const socketLogin = (socket, user) => {
-	socket.emit('login', user);
+const formClear = formIdentifier => {
+    const form = document.querySelector(formIdentifier);
 
-	socket.on('chat message', message => {
-	    const messageItem = document.createElement('li');
+    for (const el of form.children) {
+        if (el.tagName === 'INPUT' || el.tagName === 'TEXTAREA') {
+            el.value = '';
+        } 
+    }
+}
 
-	    messageItem.innerText = `${message.user.nickname}: ${message.value}`;
-	    messagesList.append(messageItem);
 
+// Смена изображения
+const handleFileSelect = e => {
+	e.stopPropagation();
+	e.preventDefault();
+
+	const [file] = e.dataTransfer.files; 
+
+	fileReader.readAsDataURL(file);
+}
+
+const handleDragOver = e => {
+	e.stopPropagation();
+	e.preventDefault();
+	e.dataTransfer.dropEffect = 'copy';
+}
+
+const changeUserImg = (nickname, url) => {
+	const images = document.querySelectorAll(`img[data-user="${nickname}"]`);
+
+	for (let img of images) {
+		img.setAttribute('src', url);
+	}
+}
+
+fileReader.addEventListener('load', () => {
+	dropZone.innerHTML = `
+		<img src="${fileReader.result}" style="width: 100%; height: 100%; object-fit: cover;" alt="${currentUser.nickname}"/>
+	`;
+	currentUser.img = fileReader.result;
+})
+
+dropZone.addEventListener('dragover', handleDragOver, false);
+dropZone.addEventListener('drop', handleFileSelect, false); 
+
+// Работа с сообщениями
+const createMessage = message => {
+	const messageItem = document.createElement('li');
+	const userImg = message.user.img ? message.user.img : 'static/img/nophoto.jpg';
+	const userImgAlt = message.user.img ? message.user.nickname : 'No photo';
+
+	messageItem.innerHTML = `
+		<div class="user">
+			<div class="user__img">
+				<img data-user="${message.user.nickname}" src="${userImg}" alt="${userImgAlt}">
+			</div>
+			<div class="user-message">
+				<span class="user-message__name">${message.user.name}</span>
+				<span class="user-message__date">${message.date}</span>
+				<p class="user-message__value">${message.value}</p>
+			</div>
+		</div>
+	`;
+
+	return messageItem;
+}
+ 
+const renderMessages = (messages, messagesList) => {
+	const messagesFragment = document.createDocumentFragment();
+
+	messages.forEach(message => {
+		const messageItem = createMessage(message);
+		messagesFragment.append(messageItem);
 	})
 
-	socket.on('login', (user, users, messages) => {
-		if (user.key === socket.id) {
-			currentUser = user;
-			currentUserNameTamplate.innerText = currentUser.name;
-			countUsersTamplate.innerText = users.length
-			usersTitle.classList.remove('hidden');
-			popup.classList.remove('popup--visible');
-		}
+	messagesList.innerHTML = '';
+	messagesList.append(messagesFragment);
 
-		const usersFragment = document.createDocumentFragment();
-
-		users.forEach(user => {
-			const userItem = document.createElement('li');
-
-			userItem.setAttribute('data-key', user.key);
-			userItem.innerText = user.name;
-			usersFragment.append(userItem);
-		})
-
-		usersList.innerHTML = '';
-		usersList.append(usersFragment);
-
-		if (messages) {
-			const messagesFragment = document.createDocumentFragment();
-
-			messages.forEach(message => {
-			    const messageItem = document.createElement('li');
-
-			    messageItem.innerText = `${message.user.name}: ${message.value}`;
-			    messagesFragment.append(messageItem);
-			})
-
-			messagesList.innerHTML = '';
-		    messagesList.append(messagesFragment);
-		}
-	})
-
-	socket.on('disconnect', (key, messages, users) => {
-		const disconnectedUser = usersList.querySelector(`li[data-key="${key}"]`);
-		console.log(users);
-
-		if (disconnectedUser) {
-			disconnectedUser.remove();
-		}
-
-		users.forEach(user => {
-			const usersFragment = document.createDocumentFragment();
-			const userItem = document.createElement('li');
-
-			userItem.setAttribute('data-key', user.key);
-			userItem.innerText = user.name;
-			usersFragment.append(userItem);
-		})
-
-		if (messages) {
-			const messagesFragment = document.createDocumentFragment();
-
-			messages.forEach(message => {
-			    const messageItem = document.createElement('li');
-
-			    messageItem.innerText = `${message.user.name}: ${message.value}`;
-			    messagesFragment.append(messageItem);
-			})
-
-			messagesList.innerHTML = '';
-		    messagesList.append(messagesFragment);
-		}
-	})
+	return true;
 }
 
 formSendMessage.addEventListener('submit', e => {
     e.preventDefault();
-    socket.emit('chat message', { value: inputMessage.value, user: currentUser } );
+
+	const date = new Date().toLocaleString();
+
+    socket.emit('chat message', { value: inputMessage.value, user: currentUser, date } );
     inputMessage.value = '';
     return false;
 });
+
+// Работа с пользователями
+const renderUsers = (users, usersList) => {
+	const usersFragment = document.createDocumentFragment();
+
+	users.forEach(user => {
+		const userItem = document.createElement('li');
+
+		userItem.setAttribute('data-key', user.key);
+		userItem.innerText = user.name;
+		usersFragment.append(userItem);
+	})
+
+	usersList.innerHTML = '';
+	usersList.append(usersFragment);
+}
 
 formLogIn.addEventListener('submit', e => {
     e.preventDefault();
@@ -125,5 +149,73 @@ formLogIn.addEventListener('submit', e => {
     return false;
 });
 
+// Работа с socet
+const socketLogin = (socket, user) => {
+	socket.emit('login', user);
+	formClear('#log-in');
+
+	userImg.addEventListener('click', e => {
+		popupFileLoad.classList.add('popup--visible');
+	})
+
+	popupFileLoad.addEventListener('click', e => {
+		const nameBtn = e.target.getAttribute('name');
+
+		if (e.target.nodeName === 'BUTTON' && nameBtn === 'load') {
+			changeUserImg(currentUser.nickname, currentUser.img);
+			socket.emit('change img', currentUser.nickname, currentUser.img);
+		}
 
 
+		if (e.target.nodeName === 'BUTTON') {
+			popupFileLoad.classList.remove('popup--visible');
+			dropZone.innerHTML = 'Перетащите сюда фото';
+		}
+	})
+
+	socket.on('change img', (user, img) => {
+		changeUserImg(user.nickname, img);
+	})
+
+	socket.on('chat message', message => {
+	    const messageItem = createMessage(message);
+
+	    messagesList.append(messageItem);
+	})
+
+	socket.on('login', (user, users, messages) => {
+		if (user.key === socket.id) {
+			currentUser = user;
+
+			if (currentUser.img) {
+				userImg.setAttribute('src', currentUser.img);
+				userImg.setAttribute('alt', currentUser.nickname);
+			}
+
+			currentUserNameTamplate.innerText = currentUser.name;
+			userImg.setAttribute('data-user', currentUser.nickname);
+			countUsersTamplate.innerText = users.length
+
+			usersTitle.classList.remove('hidden');
+			popupLogIn.classList.remove('popup--visible');
+		}
+
+		renderUsers(users, usersList);
+
+		if (messages) {
+			renderMessages(messages, messagesList);
+		}
+	})
+
+	socket.on('disconnect', (key, messages, users) => {
+		const disconnectedUser = usersList.querySelector(`li[data-key="${key}"]`);
+
+		if (disconnectedUser) {
+			disconnectedUser.remove();
+		}
+
+		if (messages) {
+			renderMessages(messages, messagesList);
+		}
+	})
+}
